3988f49c99ce17b87459373ba603eeab6f11eb0e
Md. Nuray Alam Siddik <siddik4x@gmail.com>
2021-03-28 12:40:20 +0600

    header, home, shipment component added

---
+++ src/components/Shipment/Shipment.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Shipment.css'
+const Shipment = () => {
+    return (
+        <div>
+            <h1>This shipment</h1>
+        </div>
+    );
+};
+
+export default Shipment;
